50e1741ac9749e1e0eefd58b0ddd9784a8e8b241
João Oliveira <joaoholiveira1@hotmail.com>
2019-10-15 10:14:23 -0300

    validação customizada: email não repetido na inserção de cliente

--- src/main/java/com/example/demo/repositories/ClienteRepository.java
+++ src/main/java/com/example/demo/repositories/ClienteRepository.java
@@ -1,5 +1,7 @@
 package com.example.demo.repositories;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,8 @@ import com.example.demo.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
+	
+	@Transactional
+	Cliente findByEmail(String email); 
+	
 }
