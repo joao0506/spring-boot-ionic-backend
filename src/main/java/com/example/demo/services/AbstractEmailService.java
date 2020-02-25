83bd4026ee98f2c7908446626d74618f334fb001
João Oliveira <joaoholiveira1@hotmail.com>
2020-02-24 21:54:37 -0300

    Configuração inicial do Spring Security

--- src/main/java/com/example/demo/services/AbstractEmailService.java
+++ src/main/java/com/example/demo/services/AbstractEmailService.java
@@ -46,7 +46,7 @@ public abstract class AbstractEmailService implements EmailService {
 	protected String htmlFromTemplatePedido(Pedido obj) {
 		Context context = new Context(); 
 		context.setVariable("pedido", obj);
-		return templateEngine.process("email/confirmacaoPedido", context);
+		return templateEngine.process("confirmacaoPedido", context);
 	}
 	
 	@Override
