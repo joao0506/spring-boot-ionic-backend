4913d66fc53257cb28a451e6e5065a74b52cca90
joao0506 <joaoholiveira1@hotmail.com>
2020-06-14 14:39:47 -0300

    Padronizando formato das exceções

--- src/main/java/com/example/demo/resources/exception/ValidationError.java
+++ src/main/java/com/example/demo/resources/exception/ValidationError.java
@@ -8,8 +8,8 @@ public class ValidationError extends StandardError {
 	
 	private List<FieldMessage> errors =  new ArrayList<>(); 
 	
-	public ValidationError(Integer status, String msg, Long timeStamp) {
-		super(status, msg, timeStamp);
+	public ValidationError(Long timestamp, Integer status, String error, String message, String path) {
+		super(timestamp, status, error, message, path);
 	}
 
 	public List<FieldMessage> getErrors() {
@@ -19,6 +19,4 @@ public class ValidationError extends StandardError {
 	public void addError(String fieldName, String message) {
 		errors.add(new FieldMessage(fieldName, message)); 
 	}
-
-	
 }
